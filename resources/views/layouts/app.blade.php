43a42a8e5e4d616eac654d6a71b2acfc6ed43c35
Javier "Ciberman" Mora <jhm.ciberman@gmail.com>
2025-04-15 16:38:18 -0300

    remove other unused alpine component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,17 +110,5 @@
             </div>
         </footer>
     </div>
-
-    <!-- Alpine JS initialization -->
-    <script>
-        document.addEventListener('alpine:init', () => {
-            Alpine.data('mobileMenu', () => ({
-                open: false,
-                toggle() {
-                    this.open = !this.open
-                }
-            }))
-        })
-    </script>
 </body>
 </html>
