9b9e80ae835366e106e04db8930898f0e5d79053
Javier "Ciberman" Mora <jhm.ciberman@gmail.com>
2025-04-16 14:50:18 -0300

    Improve show page

--- resources/views/rps/show.blade.php
+++ resources/views/rps/show.blade.php
@@ -45,9 +45,13 @@
                 <!-- Score -->
                 <div class="relative flex flex-col items-center">
                     <div class="flex items-center space-x-3">
-                        <div class="text-4xl font-bold {{ !$rpsMatch->isTie() && $rpsMatch->winner_id === $rpsMatch->player1_id ? 'text-red-600' : 'text-gray-800' }}">{{ $rpsMatch->player1_score }}</div>
+                        <div class="text-4xl font-bold {{ !$rpsMatch->isTie() && $rpsMatch->winner_id === $rpsMatch->player1_id ? 'text-red-600' : 'text-gray-800' }}">
+                            {{ $rpsMatch->player1_score }}
+                        </div>
                         <span class="text-xl text-gray-400">-</span>
-                        <div class="text-4xl font-bold {{ !$rpsMatch->isTie() && $rpsMatch->winner_id === $rpsMatch->player2_id ? 'text-blue-600' : 'text-gray-800' }}">{{ $rpsMatch->player2_score }}</div>
+                        <div class="text-4xl font-bold {{ !$rpsMatch->isTie() && $rpsMatch->winner_id === $rpsMatch->player2_id ? 'text-blue-600' : 'text-gray-800' }}">
+                            {{ $rpsMatch->player2_score }}
+                        </div>
                     </div>
 
                     <div class="mt-3 flex items-center justify-center">
@@ -364,7 +368,7 @@
                 </div>
 
                 <div class="mt-4 flex justify-center" x-show="!showAllRounds && {{ count($rounds) > 10 ? 'true' : 'false' }}">
-                    <button @click="showAllRounds = true" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+                    <button x-on:click="showAllRounds = true" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
                         Show all {{ count($rounds) }} rounds
                         <x-phosphor-caret-down class="ml-2 -mr-1 h-4 w-4 text-gray-400" />
                     </button>
